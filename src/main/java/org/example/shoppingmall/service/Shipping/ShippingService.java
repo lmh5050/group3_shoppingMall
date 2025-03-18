fea252d02fad4def93366cb56d4e882a0f188824
ìYeji11 <yeaji0804@gmail.com>
2025-03-18 18:49:59 +0900

    DTO file

--- src/main/java/org/example/shoppingmall/service/Shipping/ShippingService.java
+++ src/main/java/org/example/shoppingmall/service/Shipping/ShippingService.java
@@ -1,4 +1,20 @@
 package org.example.shoppingmall.service.Shipping;
 
+import org.example.shoppingmall.dto.shipping.ShippingDto;
+import org.example.shoppingmall.repository.shipping.ShippingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class ShippingService {
+    private final ShippingRepository shippingRepository;
+
+    public ShippingService(ShippingRepository shipp) {
+        this.shippingRepository = shipp;
+    }
+
+   public ArrayList<ShippingDto> shippingCompany(){
+        return shippingRepository.shippingCompany();
+    }
 }
