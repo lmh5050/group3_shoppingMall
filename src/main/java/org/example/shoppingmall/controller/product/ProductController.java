83e759c5227f5ea9ea1a6340ca423de7553551e2
kimyelin0506 <annalin1004@gmail.com>
2025-03-18 12:50:26 +0900

    index.html l형식에 맞게 고치고, DB에 있는 상품과 상품 사진들을 매핑해서 보여줘라

--- src/main/java/org/example/shoppingmall/controller/product/ProductController.java
+++ src/main/java/org/example/shoppingmall/controller/product/ProductController.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class ProductController {
@@ -22,15 +18,16 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping
+    @GetMapping("/")
     public String home(Model model) {
         ArrayList<ProductDto> products = productService.getProductData();
         model.addAttribute("products", products);
         return "index";
     }
 
-    @GetMapping("/product")
-    public String productDetail(Model model) {
-        return "product";
+    @GetMapping("/productDetail")
+    public String productDetail(String prdId) {
+
+        return "indexDetail";
     }
 }
