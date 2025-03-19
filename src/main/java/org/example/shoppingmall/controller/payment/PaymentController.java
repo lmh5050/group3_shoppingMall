630a546255e43f8a91cf9ed4d8d3db8b95777a37
Supermoon-JH <dknl133@gmail.com>
2025-03-20 04:20:57 +0900

    feat: 결제 팝업 페이지 구현

--- src/main/java/org/example/shoppingmall/controller/payment/PaymentController.java
+++ src/main/java/org/example/shoppingmall/controller/payment/PaymentController.java
@@ -18,32 +18,26 @@ import java.util.List;
 public class PaymentController {
     private final PaymentService paymentService;
 
-    // 결제 데이터 Get 테스트
-    @GetMapping
-    public List<?> getPaymentPage() {
-        return paymentService.getPayments();
-    }
-
     // 결제버튼이 보이는 페이지
-    @GetMapping("/page/{orderId}")
-    public String paymentPage(@PathVariable String orderId, Model model){
-        PaymentInfoDto order = paymentService.getOrderDetails(orderId);
-        model.addAttribute("order", order);
+    @GetMapping("/page")
+    public String paymentPage(Model model){
+        model.addAttribute("orderId", "OR001");
+        model.addAttribute("totalOrderAmount", 50000);
         return "payment/payment";
     }
 
     // 결제 팝업 페이지
-    @GetMapping("/popup")
-    public String paymentPopup(@RequestParam String orderId, Model model) {
-        PaymentInfoDto order = paymentService.getOrderDetails(orderId);
-        model.addAttribute("order", order);
+    @GetMapping
+    public String paymentPopup(Model model) {
+//        PaymentInfoDto order = paymentService.getOrderDetails(orderId);
+//        model.addAttribute("order", order);
         return "payment/payment-popup";
     }
 
     // 결제하기
     @PostMapping
     public ResponseEntity<String> postPayment(@RequestBody PaymentDto paymentDto) {
-        paymentService.createPayment(paymentDto);
+        // paymentService.createPayment(paymentDto);
         return ResponseEntity.ok("created payment");
     }
 }
