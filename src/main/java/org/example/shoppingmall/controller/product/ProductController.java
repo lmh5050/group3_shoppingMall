25c24b31cb8db1e6bac673652fa85684a6045ebd
kimyelin0506 <annalin1004@gmail.com>
2025-03-20 15:17:59 +0900

    카테고리 디자인, 상품 띄ì워라

--- src/main/java/org/example/shoppingmall/controller/product/ProductController.java
+++ src/main/java/org/example/shoppingmall/controller/product/ProductController.java
@@ -29,6 +29,7 @@ public class ProductController {
     public String home(Model model) {
         ArrayList<ProductDto> products = productService.getProductData();
         model.addAttribute("products", products);
+
         ArrayList<ProductCategoryDto> list = productCategoryService.getMajorCategoryByPId();
         model.addAttribute("categoryList", list);
 
@@ -52,15 +53,28 @@ public class ProductController {
     public String categoryList(
             @RequestParam(name = "majorCID") String majorCID,
             @RequestParam(required = false, name = "midCID") String midCID,
+            @RequestParam(required = false, name = "subCID") String subCID,
             Model model) {
+
         ArrayList<ProductCategoryDto> midCList = productCategoryService.getMiddleCategoryByPId(majorCID, "mid");
         model.addAttribute("midCategoryList", midCList);
+
+        ArrayList<ProductCategoryDto> list = productCategoryService.getMajorCategoryByPId();
+        model.addAttribute("categoryList", list);
+
+        // 중분류 카테고리가 선택된 경우
         ArrayList<ProductCategoryDto> subCList;
         if(midCID != null) {
             subCList = productCategoryService.getMiddleCategoryByPId(midCID, "sub");
-            System.out.println("subCID = " + subCList);
             model.addAttribute("subCategoryList", subCList);
         }
+
+        // 소분류 카테고리가 선택된 경우
+        if(subCID != null) {
+            ArrayList<ProductDto> products = productService.getProductData();
+            model.addAttribute("products", products);
+        }
+        
         return "/product/category";
     }
 }
