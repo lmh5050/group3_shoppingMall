7b2860f6af4a546867f9503834fe2521bf8a5454
ìspace <dlgkfn04@gmail.com>
2025-03-24 10:03:48 +0900

    주orí테스트 위한 저장

--- src/main/java/org/example/shoppingmall/service/order/OrderListService.java
+++ src/main/java/org/example/shoppingmall/service/order/OrderListService.java
@@ -5,6 +5,7 @@ import org.example.shoppingmall.dto.order.OrderListDto;
 import org.example.shoppingmall.repository.order.OrderListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,12 +18,15 @@ public class OrderListService {
     public List<OrderListDto> getOrderListByCustomerId(String customerId) {
         return orderListRepository.findOrderListByCustomerId(customerId);
     }
-    /*public OrderDetailDto getOrderDetailByOrderId(Long orderId) {
-        return orderListRepository.findOrderDetailByOrderId(orderId);
-    }*/
 
     public List<OrderDetailDto> getOrderDetailByOrderId(Long orderId) {
         return orderListRepository.findOrderDetailByOrderId(orderId);
+    }
+
+    @Transactional
+    public void deleteOrder(Long orderId) {
+        orderListRepository.deleteOrder(orderId);
+        orderListRepository.deleteShipping(orderId);
     }
 
 }
