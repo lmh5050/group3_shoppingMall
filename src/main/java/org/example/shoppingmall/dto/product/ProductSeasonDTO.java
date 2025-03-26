6ddaaa0f03adfdf282715cee5e2daa606baaf6f5
kimyelin0506 <annalin1004@gmail.com>
2025-03-26 20:39:34 +0900

    상품 상세 페이지 CSS 수정/추가

---
+++ src/main/java/org/example/shoppingmall/dto/product/ProductSeasonDTO.java
@@ -0,0 +1,16 @@
+package org.example.shoppingmall.dto.product;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class ProductSeasonDTO {
+    private Long productId;        // 상품 ID
+    private Long seasonId;         // 시즌 ID
+    private String name;           // 시즌 이름
+    private Integer periodMonth;   // 시작 월
+    private Integer durationPeriod; // 진행 기간
+}
