67f41f7cd4abc31a79b1d86c4b3901078e24502f
lmh5050 <94948047+lmh5050@users.noreply.github.com>
2025-03-28 14:31:13 +0900

    어제 했던 작업들

--- src/main/java/org/example/shoppingmall/controller/User/UserController.java
+++ src/main/java/org/example/shoppingmall/controller/User/UserController.java
@@ -2,9 +2,7 @@ package org.example.shoppingmall.controller.User;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.apache.catalina.User;
-import org.example.shoppingmall.dto.User.*;
-import org.example.shoppingmall.dto.order.AddressDto;
+import org.example.shoppingmall.dto.user.*;
 import org.example.shoppingmall.service.login.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,9 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Controller
